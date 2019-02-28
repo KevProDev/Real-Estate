ad512ad3d635d8d316d4233fda99690fad1983dc
developerXdecoded <34496647+developerXdecoded@users.noreply.github.com>
2019-02-27 23:49:32 -0600

    no message

--- webpack.config.js
+++ webpack.config.js
@@ -47,12 +47,4 @@ module.exports = {
       template: "./index.html"
     })
   ]
-//         new webpack.optimize.UglifyJsPlugin({
-//   sourceMap: options.devtool && (options.devtool.indexOf("sourcemap") >= 0 || options.devtool.indexOf("source-map") >= 0)
-// }),
-// new webpack.Define
-    // new webpack.optimize.CommonsChunkPlugin({
-    //     name: 'manifest' //But since there are no more common modules between them we end up with just the runtime code included in the manifest file
-    // })
-
 };
